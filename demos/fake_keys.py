e5ad3ac1521829ecb116e1918f1ed576b2ddc719
Hamza Hssaini <hamzahssaini0@gmail.com>
2026-05-08 23:56:20 +0100

    back to normal

--- demos/fake_keys.py
+++
@@ -1,4 +0,0 @@
-# aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
-# aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-# github_token = "ghp_xxXYyYyZzZ1234567890abcdefghijklmn"
-# stripe_api_key = "sk_placeholder_1234567890abcdefghijklmno"
