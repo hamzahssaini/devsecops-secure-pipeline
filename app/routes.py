77be1ab0e128750e71ba1037a07a7f342a29e938
Hamza Hssaini <hamzahssaini0@gmail.com>
2026-05-11 11:35:42 +0100

    test1 test trufflehug with an api key clear in the code

--- app/routes.py
+++ app/routes.py
@@ -99,7 +99,7 @@ def init_routes(app):
         # Commented out for future SAST/Bandit testing demos.
         # Uncommenting this will trigger CWE-798 (Hardcoded Secrets) and CWE-94 (Code Injection)
         # =========================================================================
-        SECRET_API_KEY = "12345-SUPER-SECRET-DEV-KEY"
+        SECRET_API_KEY = "AKIAIOSFODNN7EXAMPLE"
         # user_input = data.get('command', 'print("No command")')
         # exec(user_input)
 
