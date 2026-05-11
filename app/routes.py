3190e6b2d7471e5e2797360bbd5390296ff13477
Hamza Hssaini <hamzahssaini0@gmail.com>
2026-05-11 12:10:17 +0100

    test2 test bandit SAST test where i clear a sql injection pettern

--- app/routes.py
+++ app/routes.py
@@ -100,7 +100,6 @@ def init_routes(app):
         # Uncommenting this will trigger CWE-798 (Hardcoded Secrets) and CWE-94 (Code Injection)
         # =========================================================================
         # SECRET_API_KEY = "AKIAIOSFODNN7EXAMPLE"
-        # DATABASE_URL = "postgres://superadmin:MyUltraSecretPassword123456!@database.aws.com:5432/mydb"
         user_input = data.get('command', 'print("No command")')
         exec(user_input)
 
