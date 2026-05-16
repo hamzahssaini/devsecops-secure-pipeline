2184b3a42b7dc0a29ebec57a4dba4e2deadcffad
Hamza Hssaini <hamzahssaini0@gmail.com>
2026-05-16 12:51:37 +0100

    test 1 code clean

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,8 @@ import os
 from flask import jsonify, request, render_template
 
 
-SECRET_API_KEY = "sk_test_123456789"
-API_KEY = os.getenv("API_KEY", "secure_default_or_none")
+# SECRET_API_KEY = "sk_test_123456789"
+# API_KEY = os.getenv("API_KEY", "secure_default_or_none")
 
 notes = [
     {"id": 1, "title": "First Note", "content": "Welcome to the DevSecOps Demo API!"}
